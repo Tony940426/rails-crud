386ac084bfc3fdaeb5e680c263d5408b32d838fd
Tony Wang <tony94042@gmail.com>
2022-11-10 13:49:00 +1100

    Completed all CRUD options

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,6 +18,22 @@ class RestaurantsController < ApplicationController
     redirect_to restaurant_path(restaurant)
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(restaurant)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
   private
 
   def restaurant_params
