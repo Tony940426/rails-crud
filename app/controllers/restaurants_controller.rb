9d3ae6537fd22e1280f222c0d510ee5ea1cc0977
Tony Wang <tony94042@gmail.com>
2022-11-10 12:54:32 +1100

    Added CRUD actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,4 +4,23 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save
+    redirect_to restaurant_path(restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :rating, :address)
+  end
 end
